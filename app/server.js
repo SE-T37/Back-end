097c8fcf5b948d33272a60c37158e7a2bee039a6
matteobregola <matteobregola@gmail.com>
2022-12-12 15:34:41 +0100

    Added server connection

--- app/server.js
+++ app/server.js
@@ -1,9 +1,20 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const routes= require('../app/routes/user');
 app.use('/', routes);
+
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) return console.log("Error: ", err);
+        console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
+    }
+);
 
 const listener= app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port ' + listener.address().port)
