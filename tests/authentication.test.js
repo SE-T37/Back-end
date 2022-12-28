afad15f60a86191eda7d516e554055d2cf38f259
matteobregola <matteobregola@gmail.com>
2022-12-28 17:52:39 +0100

    auth api testing

---
+++ tests/authentication.test.js
@@ -0,0 +1,52 @@
+const request= require("supertest");
+const jwt = require ("jsonwebtoken");
+const app = require("../app/app");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery',false);
+const url= "http://localhost:8080" || process.env.HEROKU;
+
+beforeAll(async () => {
+    jest.setTimeout(8000);
+    app.locals.db = await mongoose.connect(process.env.MONGODB_URI);
+});
+
+afterAll(() => {
+    mongoose.connection.close(true);
+});    
+/*
+var token= jwt.sign({username: "testUser1", password: "password"}, 
+process.env.SUPER_SECRET, {expiresIn: 86400});
+*/
+const body1={
+    username: 'NotExistingUsername',
+    password: 'NotExistingPassword',
+}
+test('Authentication, wrong username ', ()=>{
+    return request(app).post('/authenticate')
+    .set('Accept', 'application/json')
+    .send(body1)
+    .expect(404);
+    
+})
+const body2={
+    username: 'alfredo2',
+    password: 'wrongpassword',
+}
+test('Authentication, wrong password ', ()=>{
+    return request(app).post('/authenticate')
+    .set('Accept', 'application/json')
+    .send(body2)
+    .expect(400);
+    
+})
+const body3={
+    username: 'alfredo2',
+    password: 'qwert12323h',
+}
+test('Authentication, correct ', ()=>{
+    return request(app).post('/authenticate')
+    .set('Accept', 'application/json')
+    .send(body3)
+    .expect(200);
+    
+})
