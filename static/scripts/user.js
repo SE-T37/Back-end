cee688ab441aec46bda9a476ea72f106548c8777
Riccardo Libanora <riccardo.libanora@gmail.com>
2023-01-02 11:39:52 +0100

    fixed users popup

--- static/scripts/user.js
+++ static/scripts/user.js
@@ -269,7 +269,7 @@ function generateUserEntry(userUsername, userFoto) {
     userEntry.classList.add("userentry");
 
     const avatar = document.createElement("img");
-    avatar.classList.add("avatar");
+    avatar.classList.add("avatarsmall");
     
     if (userFoto!=null && userFoto !== undefined && userFoto.length > 10) {
         avatar.src = userFoto;
